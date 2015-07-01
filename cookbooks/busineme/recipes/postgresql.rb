ecd847eeb118bf21627571528bf0e9224878c452
msfernandes <matheus.souza.fernandes@gmail.com>
2015-06-30 22:11:22 -0300

    Add postgresql configurations
    Signed-off-by: msfernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- cookbooks/busineme/recipes/postgresql.rb
+++ cookbooks/busineme/recipes/postgresql.rb
@@ -1,28 +1,27 @@
 package "postgresql"
 
-cookbook_file "/etc/postgresql/9.3/main/pg_hba.conf" do
+template "/etc/postgresql/9.4/main/pg_hba.conf" do
+	source 'pg_hba.conf.erb'
 	user 'postgres'
 	group 'postgres'
 	mode 0600
-	notifies :restart, 'service[postgresql]'
+	notifies :restart, 'service[postgresql]', :immediately
 end
 
-
-execute 'createuser:busineme' do
-	command "createuser busineme"
+DB_USER = node['config']['DATABASE']['USER']
+DB_PASSWORD = node['config']['DATABASE']['PASSWORD']
+DB_NAME = node['config']['DATABASE']['NAME']
+execute "createuser:#{DB_USER}" do
+	command "psql -c \"CREATE USER #{DB_USER} WITH PASSWORD '#{DB_PASSWORD}';\""
 	user 'postgres'
-	not_if do
-		`sudo -u postgres -i psql -c "select * from pg_user where usename = 'busineme';" | grep -c busineme`
-	end
+	not_if "sudo -u postgres psql -c \"select * from pg_user where usename = '#{DB_USER}';\" | grep -c #{DB_USER}"
 end
 
-execute 'createdb:busineme' do
-	command 'createdb busineme —owner=busineme'
+execute "createdb:#{DB_NAME}" do
+	command "createdb #{DB_NAME} --owner=#{DB_USER}"
 	user 'postgres'
-	not_if do
-		`sudo -u postgres -i psql -c "select * from pg_database where datname = 'busineme';" | grep -c busineme`
-	end
-end 
+	not_if "sudo -u postgres psql -c \"select * from pg_database where datname = '#{DB_NAME}';\" | grep -c #{DB_NAME}"
+end
 
 service 'postgresql' do
 	action [:enable, :start]
